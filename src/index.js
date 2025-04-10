b1c6c4e6b72eb662c34c176067980d4af7b12b7a
TungHavertz <phamtunganh27@gmail.com>
2025-04-10 11:34:05 +0700

    refractor code create routes, app/controller folder

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
 const { engine } = require('express-handlebars')
+const { log } = require('console')
 const app = express()
 const port = 3000
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded())
+app.use(express.json())
 
 //HTTP logger
-app.use(morgan('combined')) // Log requests in a compact format
+// app.use(morgan('combined')) // Log requests in a compact format
 
 //Template engine
 app.engine('hbs', engine({
@@ -17,15 +22,8 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'))
 
-
-
-app.get('/', (req, res) => {
-  res.render('home')
-})
-
-app.get('/news', (req, res) => {
-  res.render('news')
-})
+//Routes init
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
